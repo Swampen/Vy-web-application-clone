c51aa29dc93a06f4831869cab6f7eedec63776a7
Swampen <ole-ma@outlook.com>
2019-09-10 22:22:13 +0200

    Added missing files

---
+++ WebApplication_Vy/WebApplication_Vy/Models/DTO/ZipcodeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Vy.Models.DTO
+{
+    public class ZipcodeDTO
+    {
+        [Key]
+        public string Postalcode { get; set; }
+
+        public string Postaltown { get; set; }
+    }
+}
---
+++ WebApplication_Vy/WebApplication_Vy/Models/Entities/Zipcode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Vy.Models.Entities
+{
+    public class Zipcode
+    {
+        [Key]
+        public string Postalcode { get; set; }
+
+        public string Postaltown { get; set; }
+    }
+}
