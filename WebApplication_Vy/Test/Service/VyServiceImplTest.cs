98fba6aa5eb9232ec225399cb4628245a77dab4c
FredrikFrostad <frostad.fredrik@gmail.com>
2019-09-10 16:15:36 +0200

    Test folders

--- WebApplication_Vy/Test/Service/VyServiceImplTest.cs
+++ WebApplication_Vy/Test/Service/VyServiceImplTest.cs
@@ -1,7 +1,13 @@
-﻿namespace Test.Service
+﻿using NUnit.Framework;
+
+namespace Test.Service
 {
+    [TestFixture]
     public class VyServiceImplTest
     {
-        
+        [SetUp]
+        public void SetUp()
+        {
+        }
     }
 }
--- WebApplication_Vy/WebApplication_Vy/Db/Repositories/Contracts/VyRepository.cs
+++ WebApplication_Vy/WebApplication_Vy/Db/Repositories/Contracts/VyRepository.cs
@@ -14,6 +14,6 @@ namespace WebApplication_Vy.Db.Repositories.Contracts
 
         List<Customer> findAllCustomers();
 
-        bool newTicket(Ticket inTicket);
+        bool createTicket(Ticket inTicket);
     }
 }
