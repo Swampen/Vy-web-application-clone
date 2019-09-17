21ac86d40298b4adeb92c08c3427d8dbdc7bc865
FredrikFrostad <frostad.fredrik@gmail.com>
2019-09-17 11:28:42 +0200

    Code cleanup and formatting

--- WebApplication_Vy/Test/Service/TripServiceImplTest.cs
+++ WebApplication_Vy/Test/Service/TripServiceImplTest.cs
@@ -1,10 +1,5 @@
-﻿using System.Collections.Generic;
-using Moq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Test.MockUtil;
-using WebApplication_Vy.Db.Repositories.Contracts;
-using WebApplication_Vy.Db.Repositories.Implementation;
-using WebApplication_Vy.Models.DTO;
 using WebApplication_Vy.Service.Implementation;
 
 namespace Test.Service
@@ -16,15 +11,15 @@ namespace Test.Service
         public void SetUp()
         {
         }
-        
+
         [Test]
-        public void GetAllTripDtos_should_return_list_of_4_elements()
+        public void FindTripsMatching_shouldReturnEmptyList()
         {
             var tripServiceImpl =
-                new TripServiceImpl(TripRepositoryMock.GetAllTripDtos());
-            List<TripDTO> result = tripServiceImpl.GetAllTripDtos();
-            
-            Assert.AreEqual(result.Count, 4);
+                new TripServiceImpl(TripRepositoryMock.FindTripsMatching());
+            var result = tripServiceImpl.FindTripsMatching("Lillehammer");
+
+            Assert.AreEqual(0, result.Count);
         }
 
         [Test]
@@ -32,19 +27,19 @@ namespace Test.Service
         {
             var tripServiceImpl =
                 new TripServiceImpl(TripRepositoryMock.FindTripsMatching());
-            List<TripDTO> result = tripServiceImpl.FindTripsMatching("Stavanger");
-            
+            var result = tripServiceImpl.FindTripsMatching("Stavanger");
+
             Assert.AreEqual(result.Count, 1);
         }
 
         [Test]
-        public void FindTripsMatching_shouldReturnEmptyList()
+        public void GetAllTripDtos_shouldReturnListOf4Elements()
         {
-            var tripServiceImpl = 
-                new TripServiceImpl(TripRepositoryMock.FindTripsMatching());
-            List<TripDTO> result = tripServiceImpl.FindTripsMatching("Lillehammer");
-            
-            Assert.AreEqual(0, result.Count);
+            var tripServiceImpl =
+                new TripServiceImpl(TripRepositoryMock.GetAllTripDtos());
+            var result = tripServiceImpl.GetAllTripDtos();
+
+            Assert.AreEqual(result.Count, 4);
         }
     }
 }
