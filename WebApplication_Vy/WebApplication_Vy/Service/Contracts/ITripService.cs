89fc0dab9e2ce99d1342c7f51beefcad5ad5c67b
FredrikFrostad <frostad.fredrik@gmail.com>
2019-09-17 10:12:51 +0200

    24: Created tripservice interface and class

---
+++ WebApplication_Vy/WebApplication_Vy/Service/Contracts/ITripService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebApplication_Vy.Models.DTO;
+
+namespace WebApplication_Vy.Service.Contracts
+{
+    public interface ITripService
+    {
+        List<TripDTO> GetAllTripDtos();
+        List<TripDTO> FindTripsMatching(string query);
+    }
+}
---
+++ WebApplication_Vy/WebApplication_Vy/Service/Implementation/TripServiceImpl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using WebApplication_Vy.Models.DTO;
+using WebApplication_Vy.Service.Contracts;
+
+namespace WebApplication_Vy.Service.Implementation
+{
+    public class TripServiceImpl : ITripService
+    {
+
+        public TripServiceImpl()
+        {
+            
+        }
+        
+        public List<TripDTO> GetAllTripDtos()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<TripDTO> FindTripsMatching(string query)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
