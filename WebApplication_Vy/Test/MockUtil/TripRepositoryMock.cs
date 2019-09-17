8dc71c61d94fb1634b9d070553849af7320202bd
FredrikFrostad <frostad.fredrik@gmail.com>
2019-09-17 11:27:02 +0200

    24: Added tests for TripServiceImpl

--- WebApplication_Vy/Test/MockUtil/TripRepositoryMock.cs
+++ WebApplication_Vy/Test/MockUtil/TripRepositoryMock.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Moq;
 using WebApplication_Vy.Db.Repositories.Contracts;
 using WebApplication_Vy.Models.Entities;
 
 namespace Test.MockUtil
 {
-    public static class TripRepositoryMock
+    public class TripRepositoryMock
     {
-        public static readonly Mock<ITripRepository> MockTripRepository = new Mock<ITripRepository>();
-        public static readonly List<Trip> Trips = new List<Trip>
+        private static readonly List<Trip> Trips = new List<Trip>
         {
-            new Trip() {TripId = 1, Route = "Oslo - Trondheim", Price = 100},
-            new Trip() {TripId = 1, Route = "Oslo - Bergen", Price = 150},
-            new Trip() {TripId = 1, Route = "Stavanger - Trondheim", Price = 200},
-            new Trip() {TripId = 1, Route = "Fredrikstad - Bodø", Price = 250},
+            new Trip {TripId = 1, Route = "Oslo - Trondheim", Price = 100},
+            new Trip {TripId = 2, Route = "Oslo - Bergen", Price = 150},
+            new Trip {TripId = 3, Route = "Stavanger - Trondheim", Price = 200},
+            new Trip {TripId = 4, Route = "Fredrikstad - Bodø", Price = 250}
         };
 
-        public static ITripRepository GetMockRepository()
+        public static ITripRepository GetAllTripDtos()
         {
-            MockTripRepository.Setup(mock => mock.FindAllTrips()).Returns(Trips);
-            return MockTripRepository.Object;
+            var mockTripRepository = new Mock<ITripRepository>();
+            mockTripRepository.Setup(mock => mock.FindAllTrips()).Returns(Trips);
+            return mockTripRepository.Object;
+        }
+
+        public static ITripRepository FindTripsMatching()
+        {
+            var mockTripRepository = new Mock<ITripRepository>();
+            mockTripRepository
+                .Setup(mock => mock.TripSearch(It.IsAny<string>()))
+                .Returns((string query) => Trips
+                    .Where(trip => trip.Route.Contains(query))
+                    .ToList());
+
+            return mockTripRepository.Object;
         }
     }
 }
--- WebApplication_Vy/Test/Service/TripServiceImplTest.cs
+++ WebApplication_Vy/Test/Service/TripServiceImplTest.cs
@@ -1,7 +1,10 @@
-﻿using Moq;
+﻿using System.Collections.Generic;
+using Moq;
 using NUnit.Framework;
+using Test.MockUtil;
 using WebApplication_Vy.Db.Repositories.Contracts;
 using WebApplication_Vy.Db.Repositories.Implementation;
+using WebApplication_Vy.Models.DTO;
 using WebApplication_Vy.Service.Implementation;
 
 namespace Test.Service
@@ -9,17 +12,39 @@ namespace Test.Service
     [TestFixture]
     public class TripServiceImplTest
     {
-        private ITripRepository _tripRepository = Mock<TripRepositoryImpl>;
         [SetUp]
         public void SetUp()
         {
-            
         }
         
         [Test]
-        public void GetAllTripDtos()
+        public void GetAllTripDtos_should_return_list_of_4_elements()
         {
-            TripServiceImpl tripServiceImpl = new TripServiceImpl();
+            var tripServiceImpl =
+                new TripServiceImpl(TripRepositoryMock.GetAllTripDtos());
+            List<TripDTO> result = tripServiceImpl.GetAllTripDtos();
+            
+            Assert.AreEqual(result.Count, 4);
+        }
+
+        [Test]
+        public void FindTripsMatching_shouldReturnListOfOneElement()
+        {
+            var tripServiceImpl =
+                new TripServiceImpl(TripRepositoryMock.FindTripsMatching());
+            List<TripDTO> result = tripServiceImpl.FindTripsMatching("Stavanger");
+            
+            Assert.AreEqual(result.Count, 1);
+        }
+
+        [Test]
+        public void FindTripsMatching_shouldReturnEmptyList()
+        {
+            var tripServiceImpl = 
+                new TripServiceImpl(TripRepositoryMock.FindTripsMatching());
+            List<TripDTO> result = tripServiceImpl.FindTripsMatching("Lillehammer");
+            
+            Assert.AreEqual(0, result.Count);
         }
     }
 }
