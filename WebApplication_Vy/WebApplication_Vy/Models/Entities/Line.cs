f1145c2c236800bd2eed0f565b21051c99bc9380
Swampen <ole-ma@outlook.com>
2019-09-19 13:24:32 +0200

    Added line and tripInterval

---
+++ WebApplication_Vy/WebApplication_Vy/Models/Entities/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Vy.Models.Entities
+{
+    public class Line
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual Station Departure_Station { get; set; }
+        public virtual Station Arrival_Station { get; set; }
+    }
+}
---
+++ WebApplication_Vy/WebApplication_Vy/Models/Entities/TripInterval.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Vy.Models.Entities
+{
+    public class TripInterval
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Monday { get; set; }
+        public string Tuesday { get; set; }
+        public string Wednesday { get; set; }
+        public string Thursday { get; set; }
+        public string Friday { get; set; }
+        public string Saturday { get; set; }
+        public string Sunday { get; set; }
+    }
+}
