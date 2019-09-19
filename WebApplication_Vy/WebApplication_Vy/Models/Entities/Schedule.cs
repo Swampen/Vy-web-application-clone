1aeae6f234954e3db76a8f0a8c78b89320d57a63
Swampen <ole-ma@outlook.com>
2019-09-19 13:47:38 +0200

    Added Schedule

---
+++ WebApplication_Vy/WebApplication_Vy/Models/Entities/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Vy.Models.Entities
+{
+    public class Schedule
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual Trip Trip { get; set; }
+        public virtual Station Station { get; set; }
+        public string Arrival_Time { get; set; }
+        public string Departure_Time { get; set; }
+    }
+}
