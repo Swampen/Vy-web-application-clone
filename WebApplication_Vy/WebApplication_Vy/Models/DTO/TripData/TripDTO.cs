8cdd7739c072b818d825be95f7fa393410be51a2
FredrikFrostad <frostad.fredrik@gmail.com>
2019-09-26 12:39:31 +0200

    33: Cuastomer registration
    + Changed old index view to CustomerDetails
    + Rewrote CustomerDTO
    + Added link for testing new view

--- WebApplication_Vy/WebApplication_Vy/Models/DTO/TripData/TripDTO.cs
+++ WebApplication_Vy/WebApplication_Vy/Models/DTO/TripData/TripDTO.cs
@@ -8,19 +8,14 @@ namespace WebApplication_Vy.Models.DTO.TripData
 {
     public class TripDTO
     {
-        /*[Display(Name = "Route")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is required")]*/
-        [Key]
-        public int Id { get; set; }
-
-        public virtual LineDTO Line { get; set; }
-
-        public virtual TripIntervalDTO TripInterval { get; set; }
-
-        public virtual List<ScheduleDTO> Schedules { get; set; }
-
+        public string Departure_Station { get; set; }
+        public string Arrival_Station { get; set; }
+        public string Date { get; set; }
         public string Departure_Time { get; set; }
-
         public string Arrival_Time { get; set; }
+        public string Duration { get; set; }
+        public string Train_Changes { get; set; }
+        public int Price { get; set; }
+        public string PriceType { get; set; }
     }
 }
