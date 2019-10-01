35ddbcf0c50b15c6228ddca7dc5fdd35e175228a
FredrikFrostad <frostad.fredrik@gmail.com>
2019-10-01 10:52:17 +0200

    HOTFIX Arrival/Departure time

--- WebApplication_Vy/WebApplication_Vy/Models/DTO/SubmitPurchaseDTO.cs
+++ WebApplication_Vy/WebApplication_Vy/Models/DTO/SubmitPurchaseDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace WebApplication_Vy.Models.DTO
+﻿using System.Collections.Generic;
+
+namespace WebApplication_Vy.Models.DTO
 {
     public class SubmitPurchaseDTO
     {
         public TicketDTO Ticket { get; set; }
+        public List<TicketDTO> Tickets { get; set; }
     }
 }
