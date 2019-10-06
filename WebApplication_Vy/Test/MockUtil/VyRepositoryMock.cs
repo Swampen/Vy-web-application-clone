2d4391ae651631d6a36315febe7302bff15dc952
FredrikFrostad <frostad.fredrik@gmail.com>
2019-10-06 21:21:37 +0200

    Vyservice tests

---
+++ WebApplication_Vy/Test/MockUtil/VyRepositoryMock.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using WebApplication_Vy.Db.Repositories.Contracts;
+using WebApplication_Vy.Models.DTO;
+using WebApplication_Vy.Models.Entities;
+
+namespace Test.MockUtil
+{
+    public static class VyRepositoryMock
+    {
+        
+        private static readonly CreditCard Card = new CreditCard();
+        private static readonly List<Ticket> Tickets = new List<Ticket>
+        {
+            new Ticket{CreditCard = Card},
+            new Ticket{CreditCard = Card}
+        };
+        private static readonly Zipcode Zipcode = new Zipcode{Postalcode = "2022", Postaltown = "Gjerdrum"};
+        private static readonly List<Customer> Customers = new List<Customer>
+        {
+            new Customer{Id = 1, Address = "Adress1", Email = "1@email.com", Givenname = "Given1", Surname = "Sur1", Tickets = Tickets, Zipcode = Zipcode}, 
+            new Customer{Id = 2, Address = "Adress2", Email = "2@email.com", Givenname = "Given2", Surname = "Sur2", Tickets = Tickets, Zipcode = Zipcode},
+            new Customer{Id = 3, Address = "Adress3", Email = "3@email.com", Givenname = "Given3", Surname = "Sur3", Tickets = Tickets, Zipcode = Zipcode},
+        };
+
+        public static IVyRepository GetAllCustomerDtos()
+        {
+            var mockRepo = new Mock<IVyRepository>();
+            mockRepo.Setup(mock => mock.FindAllCustomers()).Returns(Customers);
+            return mockRepo.Object;
+        }
+
+        public static IVyRepository CreateTicketMock()
+        {
+            var mockRepo = new Mock<IVyRepository>();
+            mockRepo.Setup(mock => mock.CreateTicket(It.IsAny<Ticket>())).Returns(true);
+            return mockRepo.Object;
+        }
+
+        public static IVyRepository DeleteTicketMock()
+        {
+            var mockRepo = new Mock<IVyRepository>();
+            mockRepo.Setup(mock => mock.DeleteTicket(It.IsAny<int>())).Returns(true);
+            return mockRepo.Object;
+        }
+/*
+        public static ITripRepository FindTripsMatching()
+        {
+            var mockTripRepository = new Mock<ITripRepository>();
+            mockTripRepository
+                .Setup(mock => mock.TripSearch(It.IsAny<string>()))
+                .Returns((string query) => Trips
+                    .Where(trip => trip.Route.Contains(query))
+                    .ToList());
+
+            return mockTripRepository.Object;
+        }
+        */
+    }
+}
--- WebApplication_Vy/Test/Service/VyServiceImplTest.cs
+++ WebApplication_Vy/Test/Service/VyServiceImplTest.cs
@@ -1,13 +1,114 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Internal.Commands;
+using Test.MockUtil;
+using WebApplication_Vy.Db.Repositories.Implementation;
+using WebApplication_Vy.Models.DTO;
+using WebApplication_Vy.Service.Contracts;
+using WebApplication_Vy.Service.Implementation;
 
 namespace Test.Service
 {
     [TestFixture]
     public class VyServiceImplTest
     {
+        private IVyService _service;
+        private CardDto _cardDto;
+        private CustomerDto _customerDto;
+        private TicketDto _ticketDto;
+        private ZipcodeDto _zipcodeDto;
+        
         [SetUp]
         public void SetUp()
         {
+            _cardDto = new CardDto
+            {
+                Card_Holder = "TestHolder",
+                Card_Number = "1111 2222 3333 4444",
+                Cvc = "123",
+                Expiry_Date = "01-01-2000"
+            };
+            
+            _zipcodeDto = new ZipcodeDto
+            {
+                Postalcode = "2022",
+                Postaltown = "Gjerdrum"
+            };
+            
+            _customerDto = new CustomerDto
+            {
+                Address = "Address",
+                Email = "mail@mail.com",
+                Givenname = "Given",
+                Surname = "Sur",
+                Id = 1,
+                Tickets = null,
+                Zipcode = _zipcodeDto,
+            };
+            
+            _ticketDto = new TicketDto
+            {
+                ArrivalStation = "Arrival",
+                CreditCard = _cardDto,
+                ArrivalTime = "Arrival",
+                Customer = _customerDto,
+                DepartureStation = "Departure",
+                DepartureTime = "DepartureTime",
+                Duration = "Duration",
+                Id = 1,
+                Price = 100,
+                TrainChanges = ""
+            };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _service = null;
+            _cardDto = null;
+            _customerDto = null;
+            _ticketDto = null;
+            _zipcodeDto = null;
+        }
+
+        [Test]
+        public void MaskCreditCardNumberTest()
+        {
+            _service = new VyServiceImpl(new VyRepositoryImpl());
+            string before = _cardDto.Card_Number;
+            string expected = "**** **** **** 4444";
+            _service.MaskCreditCardNumber(_cardDto);
+            Assert.AreNotEqual(before, _cardDto.Card_Number);
+            Assert.AreEqual(expected, _cardDto.Card_Number);
+        }
+
+        [Test]
+        public void GetCustomerDtosTest()
+        {
+            _service = new VyServiceImpl(VyRepositoryMock.GetAllCustomerDtos());
+            Assert.AreEqual(3, _service.GetCustomerDtos().Count);
+            Assert.NotNull(_service.GetCustomerDtos().Find(dto => dto.Id == 1));
+        }
+
+        [Test]
+        public void GetTicketDtosTest()
+        {
+            _service = new VyServiceImpl(VyRepositoryMock.GetAllCustomerDtos());
+            Assert.IsInstanceOf(typeof(TicketDto), _service.GetTicketDtos()[0]);
+            Assert.AreEqual(6, _service.GetTicketDtos().Count);
+        }
+
+        [Test]
+        public void CreateTicketTest()
+        {
+            _service = new VyServiceImpl(VyRepositoryMock.CreateTicketMock());
+            Assert.IsTrue(_service.CreateTicket(_ticketDto));
+        }
+
+        [Test]
+        public void DeleteTicketTest()
+        {
+            _service = new VyServiceImpl(VyRepositoryMock.DeleteTicketMock());
+            Assert.IsTrue(_service.DeleteTicket(1));
         }
     }
 }
