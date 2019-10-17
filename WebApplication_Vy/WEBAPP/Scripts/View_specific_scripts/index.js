2516e56a31f00e3cd1aecdbbaadbbfdf140efc63
MichaelThoresen <mimothor@hotmail.com>
2019-10-17 14:38:41 +0200

    slight cleanup

--- WebApplication_Vy/WEBAPP/Scripts/View_specific_scripts/index.js
+++ WebApplication_Vy/WEBAPP/Scripts/View_specific_scripts/index.js
@@ -189,11 +189,6 @@
 
     $('#login').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
-        var id = button[0].id // Extract info from data-* attributes
-        console.log(id)
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-
         var url = `/home/confirmLogin`
         var modal = $(this)
         modal.find("#loginBtn").attr("href", url)
