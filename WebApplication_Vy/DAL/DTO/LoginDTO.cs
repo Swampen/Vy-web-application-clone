42eb39ea00a38be617e043610d39b6973a4640d5
MichaelThoresen <32193552+MichaelThoresen@users.noreply.github.com>
2019-10-17 20:49:33 +0200

    Create LoginDTO class

---
+++ WebApplication_Vy/DAL/DTO/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DTO
+{
+    class LoginDTO
+    {
+
+        public AdminUserDTO Admin { get; set; }
+        public String CurrentLocation { get; set; }
+    }
+}
