0f76d9112a528225578155211a1aabc074b7aa62
jakob.simonsen@outlook.com <jakob.simonsen@outlook.com>
2019-10-20 12:08:12 +0200

    updates on lint

--- WebApplication_Vy/DAL/Db/Repositories/Implementation/LoginRepositoryImpl.cs
+++ WebApplication_Vy/DAL/Db/Repositories/Implementation/LoginRepositoryImpl.cs
@@ -10,14 +10,14 @@ namespace DAL.Db.Repositories.Implementation
     public class LoginRepositoryImpl : ILoginRepository
     {
 
-        public bool UserInDB(string Username, string password)
+        public bool UserInDB(string username, string password)
         {
             using (var db = new VyDbContext())
             {
                 
                 var user = db.AdminUsers.FirstOrDefault(
                     db.AdminUsers,
-                    b => b.Password == Password && b.UserName == UserName);
+                    b => b.Password == password && b.UserName == username);
                 if (user == null)
                 {
                     return false;
@@ -30,29 +30,63 @@ namespace DAL.Db.Repositories.Implementation
         }
 
 
-        public bool SuperUser(AdminUser adminUser)
+        public bool getLoginConfirmation(string Username, string Password)
         {
-            return adminUser.SuperAdmin;
+            throw new NotImplementedException();
         }
+        
 
-        public bool DoseAdminUserExcist(adminUser adminUser)
+        public bool DoseAdminUserExcist(AdminUser adminUser)
         {
             using (var db = new VyDbContext())
             {
                 var foundAdmin = Queryable.FirstOrDefault(db.AdminUsers,
-                    user => user.UserName.Equals(adminUser.UserName))
+                    user => user.UserName.Equals(adminUser.UserName));
 
                 if (foundAdmin == null)
                 {
-                    return false
+                    return false;
                 }
                 else
                 {
-                    return foundAdmin;
+                    return true;
                 }
             }
         }
-        
-        public bool RegisterUser()
+
+        public bool RegisterUser(AdminUser adminUser)
+        {
+            using (var db = new VyDbContext())
+            {
+                var userAlreadyExcist = Queryable.FirstOrDefault(db.AdminUsers,
+                    admin => admin.UserName.Equals(adminUser.UserName));
+
+                if (userAlreadyExcist != null)
+                {
+                    return false;
+                }
+                else
+                {
+                    var user = new AdminUser
+                    {
+                        UserName = adminUser.UserName,
+                        Id = adminUser.Id,
+                        Password = adminUser.Password,
+                    },
+                    try
+                    {
+                        db.AdminUsers.Add(user);
+                        db.SaveChanges();
+                        Console.WriteLine("User with username: " + adminUser.UserName);
+                        return true;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
+            }
+        }
     }
 }
