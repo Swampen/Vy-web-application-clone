4f984910d6e6efd4a696b317317f44321490490a
Swampen <ole-ma@outlook.com>
2019-10-22 22:45:09 +0200

    Modal flow now works as intended

--- WebApplication_Vy/WEBAPP/Scripts/View_specific_scripts/Tickets.js
+++ WebApplication_Vy/WEBAPP/Scripts/View_specific_scripts/Tickets.js
@@ -1,30 +1,11 @@
 ﻿$(function () {
 
-    //$("#table").on("mouseenter", ".button-row", function () {
-    //    var row = $(this)
-    //    row.css('cursor', 'pointer')
-    //    if (!row.hasClass("clicked")) {
-    //        row.css('background-color', '#e6e6e6')
-    //    }
-    //});
-
-    //$("#table").on("mouseleave", ".button-row", function () {
-    //    var row = $(this)
-    //    if (!row.hasClass("clicked")) {
-    //        row.css('background-color', '#DBF0E0')
-    //    }
-    //});
-
     $("#table").on("click", "tr", function () {
         let id = $(this)[0].dataset["id"]
         $("#tickets" + id).modal("show")
     })
 
     $("#table").find("tr").hover().css("cursor", "pointer")
-
-
-    //    row.next().slideToggle()
-    //});
 
     $('#table').DataTable({
         scrollY: 475
@@ -40,16 +21,22 @@
    
 
     $('#delete').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var id = button[0].id;
-        console.log(id);
+        const button = $(event.relatedTarget) // Button that triggered the modal
+        const id = button[0].id;
+        const cid = button[0].dataset["cid"]
+        $("#tickets" + cid).modal("hide")
 
-
-        var url = `/admin/deleteTicket?ticketId=${id}`;
-        var modal = $(this);
+        const url = `/admin/deleteTicket?ticketId=${id}`;
+        const modal = $(this);
         modal.find("#confirmBtn").attr("href", url);
+        modal.attr("data-cid", cid);
+
     });
 
+    $('#delete').on('hidden.bs.modal', function (event) {
+        const cid = $(this)[0].dataset["cid"];
+        $("#tickets" + cid).modal("show")
+    })
 
     
 
