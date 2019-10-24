8ec903a9a7627e32d47c4421b8f6c733d28507d0
FredrikFrostad <frostad.fredrik@gmail.com>
2019-10-24 14:38:17 +0400

    CHORE Changed test namespace

--- WebApplication_Vy/Test/Controllers/LoginControllerTests.cs
+++ WebApplication_Vy/Test/Controllers/LoginControllerTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WebApplication_Vy.Controllers.Tests
+namespace Test.Controllers
 {
     [TestFixture()]
     public class LoginControllerTests
