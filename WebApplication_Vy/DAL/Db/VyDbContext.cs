ecb0fad1ed60017123d82a9e18518ab6871fa0bf
Swampen <ole-ma@outlook.com>
2019-10-25 16:01:33 +0200

    Added changelog table to database

--- WebApplication_Vy/DAL/Db/VyDbContext.cs
+++ WebApplication_Vy/DAL/Db/VyDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using MODEL.Models.Entities;
 using System;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Web;
 using System.Xml.Linq;
@@ -23,8 +25,8 @@ namespace DAL.Db
         public DbSet<Zipcode> Zipcodes { get; set; }
         public DbSet<Station> Stations { get; set; }
         public DbSet<CreditCard> CreditCards { get; set; }
-
         public DbSet<AdminUser> AdminUsers { get; set; }
+        public DbSet<ChangeLog> ChangeLogs { get; set; }
 
         public class VyDbInitializer<T> : CreateDatabaseIfNotExists<VyDbContext>
         {
@@ -95,6 +97,46 @@ namespace DAL.Db
             }
         }
 
+        public override int SaveChanges()
+        {
+            var modifiedEntities = ChangeTracker.Entries()
+                .Where(p => p.State == EntityState.Modified).ToList();
+            var now = DateTime.UtcNow;
+
+            foreach (var change in modifiedEntities)
+            {
+                var entityName = change.Entity.GetType().Name;
+                var primaryKey = GetPrimaryKeyValue(change);
+
+                foreach (var prop in change.OriginalValues.PropertyNames)
+                {
+                    var originalValue = change.OriginalValues[prop].ToString();
+                    var currentValue = change.CurrentValues[prop].ToString();
+                    if (originalValue != currentValue)
+                    {
+                        ChangeLog log = new ChangeLog()
+                        {
+                            EntityName = entityName,
+                            PrimaryKeyValue = primaryKey.ToString(),
+                            PropertyName = prop,
+                            OldValue = originalValue,
+                            NewValue = currentValue,
+                            DateChanged = now
+                        };
+                        ChangeLogs.Add(log);
+                    }
+                }
+            }
+            return base.SaveChanges();
+
+
+        }
+
+        object GetPrimaryKeyValue(DbEntityEntry entry)
+        {
+            var objectStateEntry = ((IObjectContextAdapter)this).ObjectContext.ObjectStateManager.GetObjectStateEntry(entry.Entity);
+            return objectStateEntry.EntityKey.EntityKeyValues[0].Value;
+        }
 
     }
 }
