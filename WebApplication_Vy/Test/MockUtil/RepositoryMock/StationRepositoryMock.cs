354f3b59d5123cc709ebcd6f1fc509fa72a6eb30
FredrikFrostad <frostad.fredrik@gmail.com>
2019-10-25 07:24:17 +0400

    108: Stationservice 100% coverage

--- WebApplication_Vy/Test/MockUtil/RepositoryMock/StationRepositoryMock.cs
+++ WebApplication_Vy/Test/MockUtil/RepositoryMock/StationRepositoryMock.cs
@@ -10,10 +10,10 @@ namespace Test.MockUtil.RepositoryMock
     {
         private static List<Station> _stations = new List<Station>
         {
-            new Station{Id = 1, Name = "test1", StopId = "1"},
-            new Station{Id = 2, Name = "test2", StopId = "2"},
-            new Station{Id = 3, Name = "test3", StopId = "3"},
-            new Station{Id = 4, Name = "test4", StopId = "4"}
+            new Station{Id = 1, Name = "1", StopId = "1"},
+            new Station{Id = 2, Name = "2", StopId = "2"},
+            new Station{Id = 3, Name = "3", StopId = "3"},
+            new Station{Id = 4, Name = "4", StopId = "4"}
         }; 
         public static IStationRepository FindAllStationsMock()
         {
--- WebApplication_Vy/Test/Service/StationServiceImplTest.cs
+++ WebApplication_Vy/Test/Service/StationServiceImplTest.cs
@@ -1,4 +1,10 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using BLL.Service.Implementation;
+using DAL.Db.Repositories.Contracts;
+using DAL.Db.Repositories.Implementation;
+using DAL.DTO;
+using NUnit.Framework;
+using Test.MockUtil.RepositoryMock;
 
 namespace Test.Service
 {
@@ -7,7 +13,40 @@ namespace Test.Service
         [Test]
         public void GetAllKeyValueStations_shouldReturnDictionary()
         {
+            //Arrange
+            var service = new StationServiceImpl(StationRepositoryMock.FindAllStationsMock());
             
+            //Act
+            var actual = service.getAllKeyValueStations();
+            
+            //Assert
+            Assert.IsInstanceOf<Dictionary<string, string>>(actual);
+            Assert.AreEqual(4, actual.Count);
+            foreach (var station in actual)
+            {
+                Assert.AreEqual(station.Key, station.Value);
+            }
+        }
+
+        [Test]
+        public void getAllStations_shouldReturnList()
+        {
+            //Arrange
+            var service = new StationServiceImpl(StationRepositoryMock.FindAllStationsMock());
+            
+            //Act
+            var actual = service.getAllStations();
+            
+            //Assert
+            Assert.IsInstanceOf<List<StationDTO>>(actual);
+            Assert.AreEqual(4, actual.Count);
+            foreach (var station in actual)
+            {
+                Assert.IsInstanceOf<StationDTO>(station);
+                Assert.IsNotNull(station.Id);
+                Assert.IsNotNull(station.StopId);
+                Assert.IsNotNull(station.Name);
+            }
         }
     }
 }
