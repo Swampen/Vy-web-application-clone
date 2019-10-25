3767dbcb40d72bde7bdb128843162af0e4b265bd
FredrikFrostad <frostad.fredrik@gmail.com>
2019-10-25 08:52:48 +0400

    104: Admincontroller test scaffolding

---
+++ WebApplication_Vy/Test/Controllers/AdminControllerTest.cs
@@ -0,0 +1,134 @@
+﻿using NUnit.Framework;
+
+namespace Test.Controllers
+{
+    [TestFixture]
+    public class AdminControllerTest
+    {
+        [Test]
+        public void Index_shouldReturnAdminIndexView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Index_shouldReturnUserIndexView()
+        {
+            //Auth is null or false
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Tickets_shouldReturnCustomersView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Tickets_shouldReturnUserIndexView()
+        {
+            //Auth is null or false
+            Assert.Fail();
+        }
+
+        [Test]
+        public void DeleteTicket_shouldReturnTicketsView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+
+        [Test]
+        public void DeleteTicket_shouldReturnUserIndexView()
+        {
+            //Auth is null or false
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Stations_shouldReturnStationsView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Stations_shouldReturnUserIndexView()
+        {
+            // Auth is null or false
+            Assert.Fail();
+        }
+
+        [Test]
+        public void UpdateStations_shouldReturnStationsView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+
+        [Test]
+        public void UpdateStations_shouldReturnUserIndexView()
+        {
+            //Auth is null or false
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Customers_shouldReturnCustomersView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Customers_shouldReturnUserIndexView()
+        {
+            // Auth is null or false
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void UpdateCustomer_shouldReturnCustomersView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void UpdateCustomer_shouldReturnUserIndexView()
+        {
+            // Auth is null or false
+            Assert.Fail();
+        }       
+        
+        [Test]
+        public void DeleteCustomer_shouldReturnCustomersView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void DeleteCustomer_shouldReturnUserIndexView()
+        {
+            // Auth is null or false
+            Assert.Fail();
+        }       
+        
+        [Test]
+        public void Admins_shouldReturnCustomersView()
+        {
+            //Auth is set and true
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Admins_shouldReturnUserIndexView()
+        {
+            // Auth is null or false
+            Assert.Fail();
+        }
+    }
+}
