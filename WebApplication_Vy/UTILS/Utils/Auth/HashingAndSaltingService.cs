5280406765238b3acacb9f6f0c498361021f59e9
FredrikFrostad <frostad.fredrik@gmail.com>
2019-10-26 10:06:14 +0400

    112: logging for hashingandsaltingservice

--- WebApplication_Vy/UTILS/Utils/Auth/HashingAndSaltingService.cs
+++ WebApplication_Vy/UTILS/Utils/Auth/HashingAndSaltingService.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.Security.Cryptography;
+using log4net;
+using UTILS.Utils.Logging;
 
 namespace UTILS.Utils.Auth
 {
     public class HashingAndSaltingService
     {
+        public static readonly ILog Log = LogHelper.GetLogger();
+
         public byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
         {
+            Log.Info("Generating salted hash");
             HashAlgorithm algorithm = new SHA256Managed();
 
-            byte[] plainTextWithSaltBytes =
-              new byte[plainText.Length + salt.Length];
+            var plainTextWithSaltBytes =
+                new byte[plainText.Length + salt.Length];
 
-            for (int i = 0; i < plainText.Length; i++)
-            {
-                plainTextWithSaltBytes[i] = plainText[i];
-            }
-            for (int i = 0; i < salt.Length; i++)
-            {
-                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
-            }
+            for (var i = 0; i < plainText.Length; i++) plainTextWithSaltBytes[i] = plainText[i];
+            for (var i = 0; i < salt.Length; i++) plainTextWithSaltBytes[plainText.Length + i] = salt[i];
             return algorithm.ComputeHash(plainTextWithSaltBytes);
         }
+
         public string MakeSalt()
         {
-            byte[] randomArray = new byte[10];
+            Log.Info("Generating random salt");
+            var randomArray = new byte[10];
             string randomString;
 
             var rng = new RNGCryptoServiceProvider();
