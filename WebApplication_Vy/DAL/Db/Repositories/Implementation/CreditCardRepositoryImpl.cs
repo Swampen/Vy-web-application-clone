bff12e504abdb3f6ae2acb08f325f5969fdb9968
FredrikFrostad <frostad.fredrik@gmail.com>
2019-10-26 09:46:17 +0400

    112: Creditcardrepository logging

--- WebApplication_Vy/DAL/Db/Repositories/Implementation/CreditCardRepositoryImpl.cs
+++ WebApplication_Vy/DAL/Db/Repositories/Implementation/CreditCardRepositoryImpl.cs
@@ -1,15 +1,20 @@
 ﻿using DAL.Db.Repositories.Contracts;
 using MODEL.Models.Entities;
 using System.Linq;
+using log4net;
+using UTILS.Utils.Logging;
 
 namespace DAL.Db.Repositories.Implementation
 {
     public class CreditCardRepositoryImpl : ICreditCardRepository
     {
+
+        public static readonly ILog Log = LogHelper.GetLogger();
         public CreditCard GetCardById(int cardId)
         {
             using (var db = new VyDbContext())
             {
+                Log.Info(LogEventPrefixes.DATABASE_ACCESS + ": fethced creditcard with id: " + cardId);
                 return Queryable.FirstOrDefault<CreditCard>(db.CreditCards, card => card.Id.Equals(cardId));
             }
         }
@@ -18,6 +23,7 @@ namespace DAL.Db.Repositories.Implementation
         {
             using (var db = new VyDbContext())
             {
+                Log.Info(LogEventPrefixes.DATABASE_ACCESS + ": fetch creditcard by cardnumber");
                 return Queryable.FirstOrDefault<CreditCard>(db.CreditCards, card => card.CreditCardNumber.Equals(cardNumber));
             }
         }
@@ -26,7 +32,7 @@ namespace DAL.Db.Repositories.Implementation
         {
             using (var db = new VyDbContext())
             {
-
+                Log.Info(LogEventPrefixes.DATABASE_ACCESS + ": fetch creditcard by ticketId: " + ticketId);
                 Ticket foundTicket = Queryable.FirstOrDefault<Ticket>(db.Tickets, ticket => ticket.Id == ticketId);
                 return foundTicket?.CreditCard;
             }
